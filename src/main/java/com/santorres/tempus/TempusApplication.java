38ae10cdbcd30eaa09335de4a22f66697c724b0f
sanTorres770 <sanpez-1994@hotmail.com>
2023-05-14 19:13:42 -0500

    Se adiciona estructura del proyecto

---
+++ src/main/java/com/santorres/tempus/TempusApplication.java
@@ -0,0 +1,13 @@
+package com.santorres.tempus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TempusApplication.class, args);
+    }
+
+}
